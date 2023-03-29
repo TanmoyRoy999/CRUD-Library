b2c1048628bf69918c27f78f311411b2961f625b
Tanmoy Roy <1792976@tcs.com>
2023-03-30 02:09:04 +0530

    Replacing file from asset

--- src/main/java/com/SpringBoot/CRUD/Dao/DaoInterface.java
+++
@@ -1,9 +0,0 @@
-package com.SpringBoot.CRUD.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.SpringBoot.CRUD.Entities.Book;
-
-public interface DaoInterface extends JpaRepository<Book, Long>{
-
-}
